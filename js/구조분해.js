2d6889484b1518eab2761e94ec04da1bc6a3a5a4
mmmnheawu93-lab <mmmnheawu93@gmail.com>
2026-04-27 14:26:02 +0900

    first commit

--- js/구조분해.js
+++
@@ -1,7 +0,0 @@
-//구조분해.js
-const fruits=[`사과`바나나``오렌지`];
-const fruit=fruite[0];
-const second=fruite[1];
-const fruit=fruite[0];
-
-const
--- js/전개연산.js
+++
@@ -1,9 +0,0 @@
-//전개연산.js
-const numbers=[1,2,3];
-const copynumbers=[...numbers];
-
-console.log(numbers===copynumbers);//false
-console.log(numbers==copynumbers[0]);//true
-
-const n1=[1,2,3];
-const n2=[4,5,6];
